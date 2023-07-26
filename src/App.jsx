efe64f0619a55889ac283968b60bd26a93ab37a7
Animesh kumbhakar <animeshkumbhakarhit2@gmail.com>
2023-07-27 01:23:28 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ const App = () => {
       />
       <ToastContainer
         position="top-right"
-        autoClose={2000}
+        autoClose={1000}
         hideProgressBar={false}
         newestOnTop={true}
         closeOnClick
