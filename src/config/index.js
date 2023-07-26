8596277de3a345a33374540816aaf2dd728b0671
Animesh kumbhakar <animeshkumbhakarhit2@gmail.com>
2023-07-27 00:58:31 +0530

    first

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
-// export const BaseUrl = "https://yalpcamp.onrender.com";
-export const BaseUrl = "http://localhost:3000";
+export const BaseUrl = "https://yalpcamp.onrender.com";
+// export const BaseUrl = "http://localhost:3000";
 export const Token = "authKey";
