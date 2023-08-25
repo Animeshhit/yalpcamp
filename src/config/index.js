29bc04e736032a95016c60d15f1952dcda479a4c
Animesh kumbhakar <animeshkumbhakarhit2@gmail.com>
2023-08-25 13:58:01 +0530

    improved

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,4 @@
-export const BaseUrl = "https://yalpcamp.onrender.com";
+// export const BaseUrl = "https://yalpcamp.onrender.com";"
+export const BaseUrl = "https://shiny-coat-pig.cyclic.cloud";
 // export const BaseUrl = "http://localhost:3000";
 export const Token = "authKey";
