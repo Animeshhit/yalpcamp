2d50cde6ac64e31f02a4b7ef3a542694b18a872c
Animesh Kumbhakar <128497576+Animeshhit@users.noreply.github.com>
2024-08-09 14:58:25 +0530

    updated api url

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 // export const BaseUrl = "https://yalpcamp.onrender.com";"
-export const BaseUrl = "https://yalpcamp-backend.cyclic.cloud";
+export const BaseUrl = "https://yalpcamp-backend.vercel.app";
 // export const BaseUrl = "http://localhost:3000";
 export const Token = "authKey";
