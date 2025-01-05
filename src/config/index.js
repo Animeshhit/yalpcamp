21b059c5e164542646f1d6ec528660dcd53edd55
Animeshhit <animeshkumbhakarhit2@gmail.com>
2025-01-06 00:17:45 +0530

    fixed

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,5 @@
 // export const BaseUrl = "https://yalpcamp.onrender.com";"
-export const BaseUrl = "https://yalpcamp-backend.vercel.app";
+export const BaseUrl = "https://yalpcamp-backend.onrender.com";
+// export const BaseUrl = "http://localhost:8080";
 // export const BaseUrl = "http://localhost:3000";
 export const Token = "authKey";
