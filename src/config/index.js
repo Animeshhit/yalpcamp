5921779137a1eb4f22187752d74ab9871a97085b
Animesh Kumbhakar <128497576+Animeshhit@users.noreply.github.com>
2025-08-16 04:27:02 +0530

    moved env to private

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 // export const BaseUrl = "https://yalpcamp.onrender.com";"
-export const BaseUrl = "https://yalpcamp-backend.onrender.com";
+export const BaseUrl = import.meta.env.VITE_SERVER;
 // export const BaseUrl = "http://localhost:8080";
 // export const BaseUrl = "http://localhost:3000";
 export const Token = "authKey";
