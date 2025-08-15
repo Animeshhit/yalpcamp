f12915b6ff56940853eafb38b053e6c3930ae08a
Animesh Kumbhakar <128497576+Animeshhit@users.noreply.github.com>
2025-08-16 03:42:33 +0530

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 // export const BaseUrl = "https://yalpcamp.onrender.com";"
-export const BaseUrl = "https://yalpcamp-backend.onrender.com";
+export const BaseUrl = "http://yalpcamp-env.eba-vc8pqatz.ap-south-1.elasticbeanstalk.com";
 // export const BaseUrl = "http://localhost:8080";
 // export const BaseUrl = "http://localhost:3000";
 export const Token = "authKey";
